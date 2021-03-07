cda9d1238ce9ba936e94c53ddaf2f90a91e5416e
John Beale <jbeale1@users.noreply.github.com>
2021-03-07 07:40:22 -0800

    Create pico-blink.py

---
+++ pico-blink.py
@@ -0,0 +1,10 @@
+# https://projects.raspberrypi.org/en/projects/getting-started-with-the-pico/5
+
+from machine import Pin, Timer
+led = Pin(25, Pin.OUT)
+timer = Timer()
+
+def blink(timer):
+    led.toggle()
+
+timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
