a3ab77e53d6ee1bbe7cd8b6e6199a90ef42e07b8
John Beale <jbeale1@users.noreply.github.com>
2021-03-17 11:09:14 -0700

    Update ESP8266-Port-Data.py

--- ESP8266-Port-Data.py
+++ ESP8266-Port-Data.py
@@ -1,27 +1,44 @@
 # Socket data transfer Tx/Rx, J.Beale March 16 2021
 # ESP8266 device is client, and expects remote server to be listening already.
-# for example running 'nc -l 8888' on remote host
+# for example running 'nc -l <port>' on remote host
 
 # https://stackoverflow.com/questions/21233340/sending-string-via-socket-python
 
-import socket, errno, time, machine, utime
+import machine        # hardware pins
+import socket, errno  # NTP via wifi
+import time    # RTC time/date stamp
+import utime   # msec() and usec()
 import uerrno  # list of symbolic OSError codes
+import ntptime # get current time of day
 
 server = '192.168.1.105'  # remote server to send data (rp49.local)
+port = 8889
+
+ntptime.settime()
+print("UTC time：%s" % str(time.localtime())) # UTC time after sync
+print("UTC epoch： ", time.time() )
+
 s = socket.socket()
 try:
-    s.connect((server,8888))  
+    s.connect((server,port))  
 
 except OSError as err:         # errno.ECONNRESET:
     if err.args[0] == uerrno.ECONNRESET:
         print("Unable to connect to %s" % server)
-    machine.soft_reset()  # halt and catch fire. Well, not really.
+    machine.soft_reset()  # 
     
+pktNumber = 0
 while True:
     try:
+      pktNumber += 1
+      ts = str(time.localtime())
+      print("%s Packet %d" % (ts,pktNumber))
+      s.send((ts+'\n').encode())
       for i in range(10):  # just send out a bunch of time data, as strings
         outs = str(utime.ticks_ms()) + ", " + str(utime.ticks_us()) + '\n'
         s.send(outs.encode())
+      s.send(("End of packet %d\n\n" % pktNumber).encode())
+        
     except OSError as err:         # errno.ECONNRESET:
       if err.args[0] == uerrno.ECONNRESET:
         print("Connection to %s reset by remote host." % server)
@@ -31,3 +48,15 @@ while True:
     time.sleep(2)
     
 s.close()
+
+"""
+Sample output on remote server:
+
+(2021, 3, 17, 17, 24, 10, 2, 76)
+3894232, 673010544
+3894240, 673017932
+3894247, 673025313
+3894255, 673032696
+...and so forth
+
+"""
