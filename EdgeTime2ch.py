094214288ce2792a01d21eb4115e5ea2d5b72de8
John Beale <jbeale1@users.noreply.github.com>
2021-03-23 19:34:14 -0700

    working with ChA PIO and ChB non-PIO

--- EdgeTime2ch.py
+++ EdgeTime2ch.py
@@ -10,23 +10,30 @@ P2       __/       \_______/       \_______/
            
 Time P1 edges (B-D, D-F) with a resolution of 2 * Tclock  (8 ns @ F=250MHz)
 also record (P1,P2) levels at 2..4 cycles after each P1 edge
+
+  For debug, connect GP22 -> GP17 and GP21 -> GP16
+    GP## numbering as per RasPi Pico pinout on p.4 of
+https://datasheets.raspberrypi.org/pico/pico-datasheet.pdf
 """
 
 import rp2                 # rp2.PIO, rp2.asm_pio
 import machine as m        # m.freq, m.Pin
-import utime               # utime.sleep
+import utime               # utime.sleep, utime.ticks
 
-MFREQ = 250000000  # CPU frequency in Hz (typ. 125 MHz; Overclock to 250 MHz)
+#MFREQ = 250000000  # CPU frequency in Hz (typ. 125 MHz; Overclock to 250 MHz)
+MFREQ = 125000000  # CPU frequency in Hz (typ. 125 MHz; Overclock to 250 MHz)
+
+selfTest = False     # generate simulated quadrature signal
 
 # ------------------------------------------------------------
 
 @rp2.asm_pio()
 def pin_timing():
 
-    mov(x,0)          # init could be done with _sm.exec() from caller   
-    wait(0,pin,0)     # wait for pin1 to be low (if isn't already)
-    wait(1,pin,0)     # wait for pin1 to be high: first rising edge    
-    jmp(x_dec,'loop1') # start main loop
+#    mov(x,0)          # init could be done with _sm.exec() from caller   
+#    wait(0,pin,0)     # wait for pin1 to be low (if isn't already)
+#    wait(1,pin,0)     # wait for pin1 to be high: first rising edge    
+#    jmp(x_dec,'loop1') # start main loop
 
     wrap_target()
     
@@ -43,6 +50,7 @@ def pin_timing():
     push()        # transfer shift register to FIFO    
     in_(pins, 2)  # read two bits (Pin1,Pin2) into ISR
     push()        # send them to FIFO (also zeroing ISR)
+    irq(0x10)        # signal data ready
 
     # ===== now pin1 is low. Dec x, exit when pin1 goes high
     
@@ -58,24 +66,47 @@ def pin_timing():
     push()        # transfer shift register to FIFO    
     in_(pins, 2)  # read two bits (Pin1,Pin2) into ISR
     push()        # send them to FIFO (also zeroing ISR)
+    irq(0x10)        # signal data ready
 
     wrap()
     
     
 # ---------------------------------------------------------------------
+def irqHandler(sm):
+    global timeData,pinData,newDataFlag,pulsein
+    (timeData,pinData) = pulsein.read()  # get the new values
+    newDataFlag = True
+    #print(pio.irq().flags())
 
+def pin2irqHandler(p):
+    global p1
+    global inState
+    global newDataFlag
+    p2v = p.value()   # Pin2: 0 if just had falling edge, 1 if had rising
+    inState = ((inState & 0b11)<<2) | (p2v<<1) | p1.value()  # update pin state variable
+    newDataFlag = 2    
+    #print('{0:04b}'.format(inState))
+    if (p2v):
+        p.irq(pin2irqHandler,m.Pin.IRQ_FALLING)  
+    else:
+        p.irq(pin2irqHandler,m.Pin.IRQ_RISING)  
+
+"""
+def irqHandler2(sm):
+        global timeData,pinData,newDataFlag,pulsein2
+        (timeData,pinData) = pulsein2.read()  # get the new values
+        newDataFlag = True
+"""
+
+# ---------------------------------------------------------------------
 class pulseTimer:
 
 # Instantiate StateMachine(0) with PIO program on Pin(16).
 
     def __init__(self, pin1, stateMachine=0):
         self.pin1 = pin1
-        self.sm = rp2.StateMachine(0, pin_timing, in_base=pin1)                
-        self.sm.init(pin_timing,freq=MFREQ,
-                     in_base=(self.pin1),
-                     jmp_pin=(self.pin1))
-        self.sm.active(1)   # start state machine running here
-        
+        self.sm = stateMachine
+
     def read_blocking(self, n):
         
         ''' in_base declares the first Pin offset
@@ -87,6 +118,14 @@ class pulseTimer:
             data.append( (0xffffffff-self.sm.get(),self.sm.get()) )
         return data
 
+    def read(self):
+        
+        ''' in_base declares the first Pin offset
+            jmp_pin declares the one pin used for jmp (not an offset)
+        '''
+        # Return tuple: (X reg timer, PIO pin states)            
+        return ( (0xffffffff-self.sm.get(),self.sm.get()) )        
+
 # -----------------------------------------
 
 def vBlink(p,t,n):      # blink LED on pin p, duration t milliseconds, repeat n times
@@ -96,56 +135,164 @@ def vBlink(p,t,n):      # blink LED on pin p, duration t milliseconds, repeat n 
         p.value(0)   # Pico, ESP32: 0 means LED off
         utime.sleep_ms(t)
 # -----------------------------------------
-          
-def main():
-    m.freq(MFREQ) # set CPU frequency; not necessarily the default 125 MHz       
 
-    p1 = m.Pin(16,m.Pin.IN, m.Pin.PULL_UP)   # Channel A / Pin1 input    
+def tickT1(timer):  # timer callback to blink ext. LED
+    global led2
+    led2.toggle()
+    #print("A:%d" % utime.ticks_ms())
+
+def tickT2(timer):  # timer callback to blink ext. LED
+    global led3, led1
+    led1.toggle()
+    led3.toggle()
+    #print("B:%d" % utime.ticks_ms())
+          
+# -----------------------------------------    
+
+def main():
+    global led1,led2,led3
+    global p1   # so p2 interrupt handler can read the pin
+    global timeData,pinData,newDataFlag
+    global pulsein, pulsein2
+    global inState  # 4-bit pin state variable (p2old,p1old,p2new,p1new)    
+        
+    m.freq(MFREQ)      # set CPU frequency; not necessarily the default 125 MHz       
+    newDataFlag = False    # haven't got any new data yet
     led1 = m.Pin(25, m.Pin.OUT)              # set pin 25 (driving onboard LED) to output
     led2 = m.Pin(22, m.Pin.OUT)              # set external output pin (driving offboard LED) to output
+    led3 = m.Pin(21, m.Pin.OUT)              # set external output pin (driving offboard LED) to output
     led1.off()
     led2.off()
+    led3.off()
+    
+    # quadrature encoder pin-state lookup, 4 bit index of last & current value of A,B inputs
+    #          0  1  2  3  4  5  6  7  8  9  10  11  12  13  14  15
+    luTable = [0,-1,+1, 0,+1, 0, 0,-1,-1, 0, 0,  +1,  0, +1, -1,  0]    
+
+    
     vBlink(led1,150,3)     # program-starting signal from onboard LED
+    print("Encoder Timer v0.02 23-March-2021 J.Beale")
     utime.sleep_ms(500)
 
-    pulsein = pulseTimer(p1) 
+    p1 = m.Pin(16,m.Pin.IN, m.Pin.PULL_UP)   # Channel A / Pin1 input
+    p2 = m.Pin(17,m.Pin.IN, m.Pin.PULL_UP)   # Channel B / Pin2 input
+    
+    sm0 = rp2.StateMachine(0, pin_timing, in_base=p1)        
+    #sm1 = rp2.StateMachine(1, pin_timing, in_base=p1)        
+    pulsein = pulseTimer(p1,sm0)          # timer to look for ChA signals
+    #pulsein2 = pulseTimer(p1,sm1)         # timer to look for ChB signals
+    sm0.init(pin_timing,freq=MFREQ,in_base=(p1),jmp_pin=(p1))        
+    #sm1.init(pin_timing,freq=MFREQ,in_base=(p1),jmp_pin=(p2))        
+    sm0.irq(irqHandler)    
+    #sm1.irq(irqHandler2)
+    sm0.exec("mov(x,0)")         # init X register before running
+    #sm1.exec("mov(x,0)")         # init X register before running
+    sm0.active(1)   # start state machines running here
+    #sm1.active(1)   # start state machines running here
+    p2.irq(pin2irqHandler,m.Pin.IRQ_FALLING)  
 
-    edges = 8   # how many edges to get at one time
+    # when PIO #0 generates interrupt, load the data from FIFO into global vars
+    #rp2.PIO(0).irq( doIRQ0() )
+
+    if selfTest:
+      #  -------------------  testing: simulate quadrature signal on output pins
+      tim1 = m.Timer()
+      tim2 = m.Timer()
+
+      tim1.init(freq=1, mode=m.Timer.PERIODIC, callback=tickT1)  # blink at this rate
+      utime.sleep_ms(500)
+      tim2.init(freq=1, mode=m.Timer.PERIODIC, callback=tickT2)  # blink at this rate
+      # --------------------------
+
+    
+    edges = 1   # how many edges to get at one time
     lcount = 0  # how many lines total sent
     pcount = 0  # how many packets sent (printed)
-    
-    (oldTicks,oldPins) = pulsein.read_blocking(1)[0]  # first call sets previous values
-    oldPins = oldPins << 2
-    
-    maxTimerCount = 1<<32  # 32 bit counter rolls over here  2^32 = 4,294,967,296
-    
-    while True:   # main loop
-        vBlink(led1,10,1)     # program-starting signal from onboard LED
-        newVals = pulsein.read_blocking(edges)  # get interval times and pin states        
-        lcount += 1
-        #print(newVals) # DEBUG
-        i = 0
-        outs = ("%d," % lcount)
-        for (n,pVal) in newVals:
-            ticks = (n + 2*i)           # +2*i correction from (mov,push,in,push) overhead
-            delta = (ticks - oldTicks) % maxTimerCount            
-            # outs += ("%d" % delta) + "," + '{0:d},{0:02b}'.format(delta,pVal)
-            pVals = (oldPins & 0b1100) | (pVal & 0b0011)
-            outs += '{0:d},{1:04b}'.format(delta,pVals)
-            oldTicks = ticks
-            oldPins = pVal << 2
-            i += 1
-            if i != edges: 
-                outs += ","
-        # outs += str(oldTicks)
-        oldTicks = (oldTicks - 2*i) % maxTimerCount
-        outs += '\n'         # end of line char concludes each line
-        # uart.write(outs)
-        print(outs,end='')
-        pcount += 1        
-    
+    encPos = 0  # current quadrature encoder position
+    lastPos = 0 # last displayed position
+    oldTicks = 0  # starting point on clock
+    ticks = 0
+    timeData=0
+    pinData=0
+    errorCount =0  # how many encoder errors detected
 
-    # sm0.active(1)   # Start the StateMachine running.
+    tLast = utime.ticks_us()    # time since printing encoder position
+    maxTimerCount = 1<<32  # 32 bit counter rolls over here  2^32 = 4,294,967,296
+
+    #(oldTicks,oldPins) = pulsein.read_blocking(1)[0]  # first call sets previous values
+    
+    print("Now waiting for new data flag.")
+    while True:          # wait for new data to arrive in background via interrupt
+        if newDataFlag:
+            break
+    print("Got flag.")
+        
+    (oldTicks,oldPins) = (timeData, pinData)
+    newDataFlag = False     # reset to prepare for next data    
+    newDataFlag = 0     # reset to prepare for next data
+    inState = oldPins & 0b11
+
+    
+# ---------  Main Loop  -----------------------------------------    
+    while True:   # main loop        
+        if newDataFlag != 0:
+            i = 0
+            if (newDataFlag == 1):  # update from Pin1 PIO state machine data
+              (n,pVal) = (timeData, pinData)
+              ticks = (n + 2*i)           # +2*i correction from (mov,push,in,push) overhead
+              delta = (ticks - oldTicks) % maxTimerCount            
+              inState = ((inState & 0b11)<<2) | (p2.value()<<1) | p1.value()  # update pin state variable
+            newDataFlag = 0                  # reset flag for next time        
+            lcount += 1        
+            #outs = ("%d," % lcount)                    
+            #outs += '{0:02d},{1:04b},{2:d}'.format(inState,inState,delta)            
+            
+            oldTicks = ticks
+            #i += 1
+            #if i != edges: 
+            #    outs += ","
+                
+            # outs += str(oldTicks)
+            oldTicks = (oldTicks - 2*i) % maxTimerCount
+            #outs += '\n'         # end of line char concludes each line
+        
+            # uart.write(outs)
+            inc = luTable[inState]
+            encPos += inc
+            if (inc == 0):
+                print("Encoder Error: state = %d" % inState)
+                errorCount += 1
+            # print("%d,%d" % (inc,inState))
+            #if abs(encPos - lastPos) > 20:
+            tNow = utime.ticks_us()
+            tDelta = (tNow - tLast) % (1<<30)
+            #b2 = (encPos >> 2) % 2
+            #b3 = (encPos >> 3) % 2
+            b4 = (encPos >> 4) % 2
+            b5 = (encPos >> 5) % 2
+            if ( b4 == 0):
+              led1.on()
+            else:
+              led1.off()            
+            if ( b5 == 0):
+                led2.on()
+            else:
+                led2.off()
+                
+            if (tDelta > 2500000):         
+              #tNow = utime.ticks_us()
+              #tDelta = (tNow - tLast) % (1<<30)
+              #print("%d,%d" % (tDelta,encPos))
+              print("%d,%d" % (encPos,errorCount))
+              lastPos = encPos
+              tLast = tNow
+            pcount += 1
+            
+        # utime.sleep_ms(5)    
+         
+    
+# ---------  End Main Loop  -----------------------------------------    
+    
 """    
     p.392 https://datasheets.raspberrypi.org/rp2040/rp2040-datasheet.pdf
     Write Register IO:CTRL Register:SM_ENABLE  set the enable bits (3..0) which starts the respective state machines.
