d25b5f62693a732781faf69ce8e48f1a2d06d61e
John Beale <jbeale1@users.noreply.github.com>
2021-03-26 16:02:42 -0700

    toggle LED, show encoder count

--- QuadHoru1.py
+++ QuadHoru1.py
@@ -4,7 +4,7 @@
 
 from machine import Pin, mem32
 from rp2 import asm_pio, StateMachine, PIO
-from time import ticks_ms, ticks_diff
+from time import ticks_ms, ticks_us, ticks_diff, sleep
 import array
 
 # Simulation SM
@@ -38,9 +38,9 @@ def counter():
     mov(y, 3)       # 4 passes for the 4 edges of quadrature signal
     wait(1, pin, 2)
     
-    wrap_target()
-    label("loop")
-    mov(x, 10) .side(1) # 10 seems to work, 0 does not... (why?)
+    wrap_target()       # -- top of main r (run-forever) loop
+    label("loop")       # --- top of inner 4-cycle loop
+    mov(x, 10) .side(0) # 10 seems to work, 0 does not... (why?)
     wait(0, pin, 2)
     in_(pins, 2)
     push()
@@ -48,64 +48,73 @@ def counter():
     jmp(pin, "output")
     jmp(x_dec, "counter_start")
     label("output")
-    mov(isr, invert(x)) .side(0)
+    mov(isr, invert(x)) .side(1)
     push()
-    jmp(y_dec, "loop")
-    irq(noblock, 0x10)   # 'block' works, but more timing cycles get lost
+    jmp(y_dec, "loop")   # --- end of inner 4-cycle loop
+    
+    irq(noblock, 0x10)   # signal 8 words of data are now read to read
     mov(y, 3) .side(0)
-    wrap()
+    wrap()               # ----- outer run-forever loop
 
-data = array.array("I", [0]*8)
-start = ticks_ms()
 def counter_handler(sm):
     global start
-    for i in range(8):
-        data[i] = sm.get()
-    tSum = (data[1]+data[3]+data[5]+data[7])  # - 499998xx
-    tNow = ticks_ms()
+    global stateEnc
+    global posEnc
+    global luTable   # encoder output lookup table
+    global led1
+    data = array.array("I", [0]*8)
+    tickSum = 0
+    for i in range(4):
+        stateEnc = ((stateEnc&0b11)<<2) | (sm.get() & 0b11)  # read chA,chB & calc. state
+        posEnc += luTable[stateEnc]         # increment current encoder position based on state
+        tickSum += sm.get()  # add up total elapsed time        
+    tNow = ticks_us()
     tDelta = ticks_diff(tNow, start) # input signal period, milliseconds
-    print("%d,%d,%d,%d,%d,%d" % (tDelta,data[0],data[2],data[4],data[6],tSum))
+    print("%d,%d,%d" % (tDelta,posEnc,tickSum))
     start = tNow
+    led1.toggle()
+    
+# --------------------------------------------
+def main():
+  global start
+  global stateEnc
+  global posEnc
+  global luTable   # encoder output lookup table
+  global led1
 
-"""
-# Instantiate and configure signal simulating SMs
-sm0 = StateMachine(0, in_sig_sim, freq=1_000_000, set_base=Pin(14))
-sm0.put(500_000) # Frequency control
-sm0.exec("pull()")
-sm0.exec("mov(isr, osr)")
-sm0.put(100_000) # Delay control
-sm0.exec("pull()")
-sm0.exec("mov(x, osr)")
-sm1 = StateMachine(1, in_sig_sim, freq=1_000_000, set_base=Pin(15))
-sm1.put(500_000) # Frequency control
-sm1.exec("pull()")
-sm1.exec("mov(isr, osr)")
-sm1.put(1) # Delay control
-sm1.exec("pull()")
-sm1.exec("mov(x, osr)")
-"""
+  led1 = Pin(25, Pin.OUT)
+  led1.off()
 
-chA = Pin(14,Pin.IN,Pin.PULL_UP)  # encoder A input signal
-chB = Pin(15,Pin.IN,Pin.PULL_UP)  # encoder B input signal
-chFlag = Pin(16)  # Flag signal, goes high for 2 cycles when chA or chB edge detected
+  posEnc=0
+  stateEnc=0
+  luTable = [0,-1,+1, 0,+1, 0, 0,-1,-1, 0, 0,  +1,  0, +1, -1,  0]  # for both pins P1,P2
+  start = ticks_us()
 
-sm2 = StateMachine(2, trigger, freq=100_000_000, in_base=chA, set_base = chFlag)
-sm2.active(1)
-sm3 = StateMachine(3, trigger, freq=100_000_000, in_base=chB, set_base = chFlag)
-sm3.active(1)
+  chA = Pin(14,Pin.IN,Pin.PULL_UP)  # encoder A input signal
+  chB = Pin(15,Pin.IN,Pin.PULL_UP)  # encoder B input signal
+  chFlag = Pin(16)  # Flag signal, goes high for 2 cycles when chA or chB edge detected
 
-sm4 = StateMachine(4, counter, freq=100_000_000, in_base=chA, jmp_pin = chFlag, sideset_base=Pin(25))
-sm4.irq(counter_handler)
+  sm2 = StateMachine(2, trigger, freq=100_000_000, in_base=chA, set_base = chFlag)
+  sm2.active(1)
+  sm3 = StateMachine(3, trigger, freq=100_000_000, in_base=chB, set_base = chFlag)
+  sm3.active(1)
 
-PIO0_BASE = 0x50200000
-PIO1_BASE = 0x50300000
-PIO_CTRL =  0x000
-SM0_EXECCTRL = 0x0cc
-SM0_SHIFTCTRL = 0x0d0
-# Join output FIFOs for sm4
-mem32[PIO1_BASE | SM0_SHIFTCTRL + 0x1000] = 1<<31
-sm4.active(1)
-# Start sm0 and sm1 in sync
-# mem32[PIO0_BASE | PIO_CTRL + 0x1000] = 0b11
+  sm4 = StateMachine(4, counter, freq=100_000_000, in_base=chA, jmp_pin = chFlag, sideset_base=Pin(22))
+  sm4.irq(counter_handler)
 
-# Note: all output is generated after Python REPL prompt returns (!)
+  PIO0_BASE = 0x50200000
+  PIO1_BASE = 0x50300000
+  PIO_CTRL =  0x000
+  SM0_EXECCTRL = 0x0cc
+  SM0_SHIFTCTRL = 0x0d0
+  # Join output FIFOs for sm4
+  mem32[PIO1_BASE | SM0_SHIFTCTRL + 0x1000] = 1<<31
+  sm4.active(1)
+  # Start sm0 and sm1 in sync
+  # mem32[PIO0_BASE | PIO_CTRL + 0x1000] = 0b11
+
+  while True:  # all the action is in the interrupt routine
+      sleep(1)
+      
+# ---------------
+main()
