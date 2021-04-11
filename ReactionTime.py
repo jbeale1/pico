40cbf2b7a25b5ec9c898082f5db15e6b76c3c2da
John Beale <jbeale1@users.noreply.github.com>
2021-04-11 11:45:02 -0700

    add false-start detection

--- ReactionTime.py
+++ ReactionTime.py
@@ -5,12 +5,12 @@ from machine import Pin  # for access to GPIO
 import urandom           # random numbers
 import time              # for microseconds elapsed time
 
-
 inA = Pin(16, Pin.IN, Pin.PULL_UP)   # use this pin for input signal
 led = Pin(25, Pin.OUT)               # set pin 25 (driving onboard LED) to output
 
 tus = time.ticks_us                  # 1 MHz timer object, tick = 1 microsecond
 led.off()                            # make sure onboard LED is off
+falseStart = 0                       # start off with no mistakes
 
 def vBlink(p,t,n):      # blink LED on pin p, duration t milliseconds, repeat n times
     for i in range(n):
@@ -19,10 +19,20 @@ def vBlink(p,t,n):      # blink LED on pin p, duration t milliseconds, repeat n 
         p.value(0)      # Pico, ESP32: 0 means LED off
         time.sleep_ms(t)
 
+def earlyTrigger(ch):
+    global falseStart
+    falseStart += 1
+    vBlink(led,50,10)    # warning blink when premature trigger
+    time.sleep_ms(1000)  # extra wait time
+
 
 def main():
+    global inA
     vBlink(led,100,2)
-    print("Reaction Time Test")
+    now = time.localtime()
+    dt_string = ("%d-%02d-%02d %02d:%02d:%02d" % now[0:6])
+    print("Reaction Time Test %s" % dt_string)
+
     totalTries = 10
     trial = 0
     lowest = 999
@@ -30,22 +40,25 @@ def main():
     while trial < totalTries:
         trial += 1
         waitTime = urandom.randrange(2000,5000)  # wait in range of this many msec
+        inA.irq(trigger=Pin.IRQ_FALLING, handler=earlyTrigger)  # start interrupt handler
         time.sleep_ms(waitTime)
-        tStart = tus()
+        inA.irq(trigger=Pin.IRQ_FALLING, handler=None)  # disable interrupt handler
+
+        tStart = tus()         # start time since LED on
         led.on()
         waitA_nEdge()          # wait for falling edge on input
-        tEnd = tus()
+        tEnd = tus()           # end time, after falling edge
         led.off()
-        delay = time.ticks_diff(tEnd, tStart)
+        delay = time.ticks_diff(tEnd, tStart)  # total reaction time
         sec = delay / 1E6      # delay in units of seconds
         print("%d  %5.3f" % (trial,sec))
         lowest = min(lowest, sec)
         sum += sec
         urandom.seed(delay)   # use variable delay to make the numbers actually random
         
-    vBlink(led,150,5)
+    vBlink(led,150,5)         # signal that we are now done
     average = (sum / trial)    
-    print("Fastest time: %5.4f  Average: %5.4f" % (lowest,average))
+    print("Fastest time: %5.4f  Average: %5.4f  Errors: %d" % (lowest,average,falseStart))
     
 # -----------------------------------------------
 
@@ -54,7 +67,6 @@ def waitA_nEdge():          # wait for falling edge on input A
         pass
     while inA.value():      # now that it's high, wait for it to go low
         pass
-
 
 # ----------------------------------------------------------------
 main()
