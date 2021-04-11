67a4aabcd16499472e33c0f6d77fb2ea4d08c929
John Beale <jbeale1@users.noreply.github.com>
2021-04-11 12:39:41 -0700

    fix error in false-start detect

--- ReactionTime.py
+++ ReactionTime.py
@@ -2,6 +2,7 @@
 # MicroPython v1.14 on Raspberry Pi Pico   J.Beale 11-April-20211
 
 from machine import Pin  # for access to GPIO
+from machine import enable_irq, disable_irq  # inside long interrupt routine 
 import urandom           # random numbers
 import time              # for microseconds elapsed time
 
@@ -22,9 +23,6 @@ def vBlink(p,t,n):      # blink LED on pin p, duration t milliseconds, repeat n 
 def earlyTrigger(ch):
     global falseStart
     falseStart += 1
-    vBlink(led,50,10)    # warning blink when premature trigger
-    time.sleep_ms(1000)  # extra wait time
-
 
 def main():
     global inA
@@ -37,11 +35,23 @@ def main():
     trial = 0
     lowest = 999
     sum = 0
+    unitWait = 10             # wait in units of this many msec
+    oldStart = falseStart     # no false starts yet
     while trial < totalTries:
         trial += 1
         waitTime = urandom.randrange(2000,5000)  # wait in range of this many msec
         inA.irq(trigger=Pin.IRQ_FALLING, handler=earlyTrigger)  # start interrupt handler
-        time.sleep_ms(waitTime)
+        loopCtr = waitTime / unitWait
+        while loopCtr > 0:
+            time.sleep_ms(unitWait)  # a short wait
+            loopCtr -= 1
+            if (falseStart > oldStart):  # did we just have a false start?
+              oldStart = falseStart
+              vBlink(led,50,10)    # warning blink when premature trigger           
+              while not inA.value():  # if still low, wait for it to go high 
+                  pass
+
+        # here, we have waited for the alloted time, so start the test
         inA.irq(trigger=Pin.IRQ_FALLING, handler=None)  # disable interrupt handler
 
         tStart = tus()         # start time since LED on
